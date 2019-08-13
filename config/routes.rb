dcd0fe4601bba3ee2a89d6d6b72f9603525b5053
Maxence POINTUD <maxence.pointud@gmail.com>
2019-08-13 13:11:47 +0200

    Implentation usernmae

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  get 'static_pages/index'
-  get 'static_pages/secret'
   devise_for :users
+
+  root 'static_pages#index'
+  get 'static_pages/secret'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
